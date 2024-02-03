86a8018e8ef804415de6c0af1bb2e14326624768
Haldies Gerhardien Pasya <haldiespasya@gmail.com>
2024-02-03 08:49:00 +0700

    day 1/366

---
+++ src/2024- Coding-Challange/1-grow.js
@@ -0,0 +1,15 @@
+
+// const grow = x => {
+//     let result = x[0];
+//     for( let i = 0; i < x.length; i++){
+//         result *= x[i];
+//     }
+//     return result;
+//   }
+
+// const grow = x => x.reduce((a, b) => a * b)
+
+const grow =_=>_.reduce((__, ___) =>__*___)
+
+console.log(grow([1,2,3,4,5,6,7,8,9,10]));
+  
---
+++ src/eksperimen/setegita.js
@@ -0,0 +1,26 @@
+const segita = (ada) => {
+    for(let i = ada; i >= 1; i--){
+        let str = ' '.repeat(ada-i);
+        let str2 = '*'.repeat(i*2 -1)
+        console.log(str + str2 + str);
+    } 
+    for(let i = 1; i <= ada; i++){
+        let str = ' '.repeat(ada-i);
+        let str2 = '*'.repeat(i*2 -1)
+        console.log(str + str2 + str);
+    } 
+    for(let i = ada; i >= 1; i--){
+        let str = ' '.repeat(ada-i);
+        let str2 = '*'.repeat(i*2 -1)
+        console.log(str + str2 + str);
+    } 
+    for(let i = 1; i <= ada; i++){
+        let str = ' '.repeat(ada-i);
+        let str2 = '*'.repeat(i*2 -1)
+        console.log(str + str2 + str);
+    } 
+}
+
+
+
+
