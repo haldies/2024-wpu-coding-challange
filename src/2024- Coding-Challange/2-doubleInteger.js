64bf57f039557e891422a8c6ce91dc00c592738f
Haldies Gerhardien Pasya <haldiespasya@gmail.com>
2024-02-05 23:21:32 +0700

    day ke 2/366

---
+++ src/2024- Coding-Challange/2-doubleInteger.js
@@ -0,0 +1,14 @@
+// wpu Coding Challenge 2024
+// 1/366
+// https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
+
+
+// <----- cara ke 1 ----->
+// function doubleInteger(i) {
+//     return i * 2;
+// }
+
+//<----- cara ke 2 ----->
+const doubleInteger = i => i * 2
+
+console.log(doubleInteger(2))
